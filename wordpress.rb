1434645b5785dffc4a21c8a7b0418a2a6b5f6e90
rahul6d <51394124+rahul6d@users.noreply.github.com>
2019-06-07 08:57:31 +0530

    Update wordpress.rb

--- wordpress.rb
+++ wordpress.rb
@@ -6,6 +6,7 @@ packages.each do |package|
  apt_package package do
    action :install
  end
+end
 service "apache2" do
 action :start
 end
@@ -17,24 +18,27 @@ remote_file "mysqlcommands" do
     path "/tmp/mysqlcommands"
 end
 execute "command" do
-command "mysql -uroot -proot < /tmp/mysqlcommands"
+command "mysql -uroot -proot < /tmp/mysqlcommands && touch /tmp/mysqlcreated"
+not_if {File.exists?("/tmp/mysqlcreated")}
 end
 remote_file "latest" do
-    source 'https://wordpress.org/latest.zip'
+   source 'https://wordpress.org/latest.zip'
     path "/tmp/latest.zip"
 end
 execute "unzip" do
   command "unzip /tmp/latest.zip -d /var/www/html"
+not_if {File.exists?("/var/www/html/wordpress/xmlrpc.php")}
 end
 remote_file "wp-config-sample" do
     source 'https://gitlab.com/roybhaskar9/devops/raw/master/coding/chef/chefwordpress/files/default/wp-config-sample.php'
     path "/var/www/html/wordpress/wp-config-sample.php"
 end
-file '/var/www/html/wordpress' do  
+directory '/var/www/html/wordpress' do  
 mode '755'
   owner 'www-data'
+group 'www-data'
 end
 execute "apache2" do
 command "service apache2 restart"
 end
-end
+#end
